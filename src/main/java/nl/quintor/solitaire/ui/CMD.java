5363481594fbff8de6bb3270f3bb687193cea682
Noah <NoahTelussa@noahs-mbp.wireless.hhs.nl>
2018-01-31 15:29:38 +0100

    added waste functions, add default functions

--- src/main/java/nl/quintor/solitaire/ui/CMD.java
+++ src/main/java/nl/quintor/solitaire/ui/CMD.java
@@ -27,15 +27,11 @@ public class CMD implements UI {
     public void refresh(GameState gameState) {
         clearScreen();
 
+        //The first card in the waste is empty
         String waste = "empty";
 
-        // display time, move, score
-        System.out.println(gameState.toString());
-        System.out.print("Waste = ");
-        System.out.println(waste);
-
-        // display columns
-        displayColumns(gameState);
+        //Run the default values like waste, collections
+        this.displayDefault(waste,gameState);
 
 
         //zet het aantal keer dat de loop voor de vraag blijft lopen op 1
@@ -47,59 +43,80 @@ public class CMD implements UI {
         //maak een nieuwe scanner voor het checken van de input
         Scanner scanner = new Scanner(System.in);
         //zet de standaard text neer
-        String controls = "D = Deck | H = Help | Q = Quit";
 
-        System.out.println(controls);
 
         //de for loop blijft net zo lang loopen totdat er een valid answer is gegeven
         for (int i = 0; i < totalTries; i++) {
-            String textInput = scanner.next().toLowerCase();
+            char textInput = scanner.next().toLowerCase().charAt(0);
             // To put a card to the waste and change the Deck
-            if (textInput.equals("d")) {
-                System.out.println(gameState.getStock());
+            if (textInput == 'd') {
 
-
-
-                if((gameState.getStock().size() - 1) == numberDeck){
-                    System.out.println(gameState.getStock().size());
-
-                    // Empty the waste
-                    emptyWaste = 1;
-                    numberDeck = 0;
-                }
-
-                if(emptyWaste == 1){
-                    waste = "empty";
-                } else {
-                    System.out.println(gameState.getStock().get(numberDeck));
-                }
-
+                //When you want a card in the deck run the function
+                //Displaywaste() With the numberDeck (What card he is at in the stock)
+                waste = displayWaste(numberDeck);
 
                 i = 0;
                 numberDeck++;
                 totalTries++;
 
-                Main main = new Main();
+                //After the function, clear screen
+                clearScreen();
+
+                //Run the default settings with the waste updated
+                displayDefault(waste,gameState);
             //To quit the game
-            } else if (textInput.equals("q")) {
+            } else if (textInput == 'q') {
                 Quit quit = new Quit();
                 System.out.println(quit.apply(gameState));
                 quit.apply(gameState);
 
             //To show the controls
-            } else if (textInput.equals("h")) {
-                System.out.println(controls);
+            } else if (textInput == 'h') {
+                displayControls();
 
                 totalTries++;
             } else {
                 //voeg een extra try toe om nog een keer door de code heen te loopen
                 System.out.println("Please try a valid character");
-                textInput = scanner.next();
+                textInput = scanner.next().charAt(0);
 
                 totalTries++;
             }
         }
 
+    }
+    private void displayControls(){
+        String controls = "D = Deck | H = Help | Q = Quit";
+
+        System.out.println(controls);
+    }
+    private void displayDefault(String waste,GameState gameState){
+        // display time, move, score
+        System.out.println(gameState.toString());
+
+        // display waste
+        System.out.print("Waste = ");
+        System.out.println(waste);
+
+        // display columns
+        displayColumns(gameState);
+
+        // display controls
+        displayControls();
+    };
+    private String displayWaste(int numberDeck){
+        GameState gameState = new GameState();
+        System.out.println(gameState.getStock());
+
+
+        if((gameState.getStock().size() - 1) == numberDeck){
+            // Empty the waste
+            numberDeck = 0;
+        }
+
+       return gameState.getStock().get(numberDeck).toShortString();
+
+        //  Main main = new Main();
     }
 
     private void displayColumns(GameState gameState) {
