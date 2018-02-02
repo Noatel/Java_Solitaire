62beb108356023fc980669112bc6b505720df031
Noah <NoahTelussa@noahs-mbp.wireless.hhs.nl>
2018-02-02 12:01:31 +0100

    begin tekst en help

--- src/main/java/nl/quintor/solitaire/ui/CMD.java
+++ src/main/java/nl/quintor/solitaire/ui/CMD.java
@@ -12,7 +12,24 @@ import java.util.Scanner;
 public class CMD implements UI {
 
     private Scanner scanner = new Scanner(System.in);
-    private String message = "";
+    private String message = "Welcome to Solitaire \n\n" +
+        "Made by: Ali Safdari, Jordie Verbakel, Tim Wapenaar en Noah Telussa \n \n" +
+        "=================================================================== \n \n" +
+        " How do i move a Card \n \n" +
+        " If you want to move a card, first you need to enter the m to move a card + \n" +
+        " After you enter the M into the commandline you need to select a card that you want to move, \n" +
+        " The cmd is asking: From, meaning from column do you want to move? \n" +
+        " After entering the column for example C1 you need to select the row.  \n" +
+        " The command line is asking you for the row, on the left side you see all rows that are available (R1 to R13) \n" +
+        " If you entered your row number you need to move the cards to a specific column, \n" +
+        " The cmd is asking where do you want to move the columns, you enter here the column  \n" +
+        " where you want to move the card to \n" +
+        "  \n" +
+        " How do i draw a card from the deck?  \n" +
+        " You draw a card from the stock to the waste by pressing the D button.\n" +
+        " After pressing the D key its cycle through all the cards from the stock until its empty \n \n"+
+        "=================================================================== \n \n" ;
+
     private String errorMessage = "";
 
     public void setMessage(String message) {
