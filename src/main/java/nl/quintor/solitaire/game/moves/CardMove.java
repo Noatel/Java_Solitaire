ebf300e9e73cbd14e964d54f5a6f274cf859e500
Ali Safdari <asafdari96@gmail.com>
2018-02-02 10:56:18 +0100

    Refactored code to use move interface

---
+++ src/main/java/nl/quintor/solitaire/game/moves/CardMove.java
@@ -0,0 +1,16 @@
+package nl.quintor.solitaire.game.moves;
+
+import nl.quintor.solitaire.game.moves.ex.MoveException;
+import nl.quintor.solitaire.models.state.GameState;
+
+public class CardMove implements Move {
+    @Override
+    public String apply(GameState gameState) throws MoveException {
+        return null;
+    }
+
+    @Override
+    public Move createInstance(String playerInput) {
+        return null;
+    }
+}
