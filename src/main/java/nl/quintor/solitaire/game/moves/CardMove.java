1ddb57d48ba9a5f74057e43ec6cc2897fbca5ad7
Ali Safdari <asafdari96@gmail.com>
2018-02-02 11:21:08 +0100

    More code refactoring

--- src/main/java/nl/quintor/solitaire/game/moves/CardMove.java
+++ src/main/java/nl/quintor/solitaire/game/moves/CardMove.java
@@ -1,16 +1,92 @@
 package nl.quintor.solitaire.game.moves;
 
 import nl.quintor.solitaire.game.moves.ex.MoveException;
+import nl.quintor.solitaire.models.deck.Deck;
+import nl.quintor.solitaire.models.deck.DeckType;
 import nl.quintor.solitaire.models.state.GameState;
+
+import java.util.Scanner;
 
 public class CardMove implements Move {
     @Override
     public String apply(GameState gameState) throws MoveException {
-        return null;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("From:");
+        String fromDeckInput = scanner.nextLine().toLowerCase();
+        Deck fromDeck = getDeckByInput(gameState, fromDeckInput);
+
+        if (fromDeck == null){
+            throw new MoveException("invalid deck, try again:");
+        }
+
+        /*while (fromDeck == null){
+            System.out.println("invalid deck, try again:");
+            fromDeckInput = scanner.nextLine().toLowerCase();
+            fromDeck = getDeckByInput(gameState, fromDeckInput);
+        }*/
+
+        String rowInput = "";
+
+        if (fromDeck.getDeckType().equals(DeckType.COLUMN)){
+            System.out.println("Row:");
+            rowInput = scanner.nextLine().toLowerCase();
+
+            if (rowInput.isEmpty() || rowInput.charAt(0) != 'r' || Integer.parseInt(rowInput.replace("r", "")) < 0 && Integer.parseInt(rowInput.replace("r", "")) > 13){
+                throw new MoveException("invalid row number, try again");
+            }
+
+            /*while (rowInput.isEmpty() || rowInput.charAt(0) != 'r' || Integer.parseInt(rowInput.replace("r", "")) < 0 && Integer.parseInt(rowInput.replace("r", "")) > 13){
+                System.out.println("invalid row number, try again");
+                rowInput = scanner.nextLine().toLowerCase();
+            }*/
+        }
+
+        System.out.println("To:");
+
+        String toDeckInput = scanner.nextLine().toLowerCase();
+        Deck toDeck = getDeckByInput(gameState, toDeckInput);
+
+        if (toDeck == null){
+            throw new MoveException("invalid target deck, try again:");
+        }
+        /*while (toDeck == null || toDeck.getDeckType().equals(DeckType.WASTE)){
+            System.out.println("invalid target deck, try again:");
+            toDeckInput = scanner.nextLine().toLowerCase();
+            toDeck = getDeckByInput(gameState, toDeckInput);
+        }*/
+
+        if (rowInput.isEmpty()) {
+            toDeck.addAll(gameState.getCardsFromDeck(fromDeck, 1));
+        }
+        else {
+            int rowNumber = Integer.parseInt(rowInput.replace("r", "")) - 1;
+            int amount = fromDeck.size() - rowNumber;
+            toDeck.addAll(gameState.getCardsFromDeck(fromDeck, amount, rowNumber));
+        }
+
+        return "Moved card(s)";
     }
 
     @Override
     public Move createInstance(String playerInput) {
-        return null;
+        return this;
+    }
+
+    private Deck getDeckByInput(GameState gameState, String input){
+        if (input == null || input.isEmpty()){
+            return null;
+        }
+
+        switch (input.charAt(0)){
+            case 'w':
+                return gameState.getWaste();
+            case 's':
+                return gameState.getStackPiles().get(input.replace("s",""));
+            case 'c':
+                return gameState.getColumns().get(input.replace("c",""));
+
+            default:
+                return null;
+        }
     }
 }
--- src/main/java/nl/quintor/solitaire/game/moves/HelpMove.java
+++ src/main/java/nl/quintor/solitaire/game/moves/HelpMove.java
@@ -6,11 +6,23 @@ import nl.quintor.solitaire.models.state.GameState;
 public class HelpMove implements Move {
     @Override
     public String apply(GameState gameState) throws MoveException {
-        return null;
+        return "\nHow do i move a Card \n" +
+            "If you want to move a card, first you need to enter the m to move a card\n" +
+            "After you enter the M into the commandline you need to select a card that you want to move,\n" +
+            "The cmd is asking: From, meaning from column do you want to move? \n" +
+            "After entering the column for example C1 you need to select the row. \n" +
+            "The command line is asking you for the row, on the left side you see all rows that are available (R1 to R13) \n" +
+            "If you entered your row number you need to move the cards to a specific column, \n" +
+            "The cmd is asking where do you want to move the columns, you enter here the column \n" +
+            "where you want to move the card to\n \n \n" +
+            "" +
+            "How do i draw a card from the deck? \n" +
+            "You draw a card from the stock to the waste by pressing the D button.\n" +
+            "After pressing the D key its cycle through all the cards from the stock until its empty \n \n";
     }
 
     @Override
     public Move createInstance(String playerInput) {
-        return null;
+        return this;
     }
 }
